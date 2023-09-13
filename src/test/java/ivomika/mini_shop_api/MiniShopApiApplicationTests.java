5c7835e2d8bf5c83f986754b0edc6b43a0d7c204
ivomika <animan.00@mail.ru>
2023-09-13 18:17:12 +0300

    init

---
+++ src/test/java/ivomika/mini_shop_api/MiniShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ivomika.mini_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
